7a9918a30935a7e5e3617eef7e1b61dd2a2cc31f
aegrshdtjfku <songoku.nerkowski3@gmail.com>
2020-05-05 12:47:20 +0200

    zadanie 0

---
+++ src/com/company/Animal.java
@@ -0,0 +1,10 @@
+package com.company;
+
+public class Animal {
+    final String species;
+    private Double weight;
+    public String name;
+
+
+    public Animal(String species) {this.species = species; }
+}
---
+++ src/com/company/Main.java
@@ -0,0 +1,16 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+        Human me = new Human();
+        me.firstName = "Paweł";
+        me.lastName = "Nerkowski";
+        me.pet = new Animal("moose");
+        me.pet.name = "Kłębek";
+
+        Animal dog = new Animal ("dog");
+        dog.name = "Akita";
+        me.pet = dog;
+        }
+}
