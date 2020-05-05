814525d2eacca77844d82626a5e6092444af1363
aegrshdtjfku <songoku.nerkowski3@gmail.com>
2020-05-05 14:33:20 +0200

    zadanie 2

---
+++ src/com/company/Car.java
@@ -0,0 +1,19 @@
+package com.company;
+
+public class Car {
+    String model;
+    String producer;
+    String engineCapacity;
+    String yearOfProduction;
+    String color;
+
+
+    public Car(String model, String producer, String engineCapacity, String yearOFProduction, String color) {
+        this.model = model;
+        this.producer = producer;
+        this.engineCapacity = engineCapacity;
+        this.yearOfProduction = yearOFProduction;
+        this.color = color;
+
+    }
+}
